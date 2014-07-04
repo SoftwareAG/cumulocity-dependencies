8e362e66aef68ab33b530ae8e3138d0458b95ad1
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-07-04 16:53:21 +0200

    additional commnets

--- osgi-dependencies/cometd/cometd-java-server/src/main/java/org/cometd/server/SessionState.java
+++ osgi-dependencies/cometd/cometd-java-server/src/main/java/org/cometd/server/SessionState.java
@@ -1,6 +1,29 @@
 package org.cometd.server;
 
 public enum SessionState {
-    UNINITILIZED, INITIALIZED, INACTIVE, ACTIVE, DISCONNECTED, TIMEOUTED
+    /**
+     * Before handshake
+     */
+    UNINITILIZED,
+    /**
+     * After handshake but no connect message yet
+     */
+    INITIALIZED,
+    /**
+     * No active long poll or no active connection
+     */
+    INACTIVE,
+    /**
+     * active long poll or active connection
+     */
+    ACTIVE,
+    /**
+     * disconnected by client
+     */
+    DISCONNECTED,
+    /**
+     * timeouted by server
+     */
+    TIMEOUTED
 
 }
