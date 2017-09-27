6c94f7c02b3f0df23ab50234f56a5fc61b24ce8f
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2017-09-27 11:50:36 +0200

    default validator disabled

--- maven-plugins/3rd-license-maven-plugin/src/main/java/com/cumulocity/maven3/plugin/thirdlicense/Generate3rdLicenseMojo.java
+++ maven-plugins/3rd-license-maven-plugin/src/main/java/com/cumulocity/maven3/plugin/thirdlicense/Generate3rdLicenseMojo.java
@@ -106,7 +106,7 @@ public class Generate3rdLicenseMojo extends AbstractMojo {
         thirdPartyLicenseFilePath.mkdirs();
         // TODO refactor as service and use plexus dependency injection
         Licenses.save(getLog(), thirdPartyFile(), jars, new JarTo3PartyInformation());
-        if (pluginContext.getBooleanProperty("validate", true)) {
+        if (pluginContext.getBooleanProperty("validate", false)) {
             // TODO refactor as service and use plexus dependency injection
             Validator.validate(getLog(), jars);
         }
