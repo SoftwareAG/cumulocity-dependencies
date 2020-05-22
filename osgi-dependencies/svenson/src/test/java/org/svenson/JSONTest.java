707fc86801b0639fe5a00b4104334c2a8e72e5a4
Kamil Sobala <kamil.sobala@cumulocity.com>
2020-05-22 11:42:24 +0200

    [MTM-30281]: Upgrade junit in tenant-management

--- osgi-dependencies/svenson/src/test/java/org/svenson/JSONTest.java
+++ osgi-dependencies/svenson/src/test/java/org/svenson/JSONTest.java
@@ -1,11 +1,11 @@
 package org.svenson;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class JSONTest {
 
