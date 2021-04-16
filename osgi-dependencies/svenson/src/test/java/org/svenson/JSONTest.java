7c82f6b3e420268b93548d0e61ccc3390317e368
Jakub Kotorowicz <jakub.kotorowicz@softwareag.com>
2021-04-16 16:09:53 +0200

    Revert "MTM-38052 change svenson Tokenizer to parse floating point numbers into Double, and not BigDecimal. BigDecimal is reserved for integers too big for Long type."
    This reverts commit c6e69f354c4a890a1c5097a8d8be3332d57891c9.

---
+++ osgi-dependencies/svenson/src/test/java/org/svenson/JSONTest.java
@@ -0,0 +1,40 @@
+package org.svenson;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class JSONTest {
+
+    @Test
+    public void test() {
+        Map<String, Object> test = new HashMap<String, Object>();
+        test.put("self", "http://integration.cumulocity.com/inventory/managedObjects/10200/assetParents");
+
+        String parsedTest = JSON.defaultJSON().forValue(test);
+
+        assertEquals("{\"self\":\"http://integration.cumulocity.com/inventory/managedObjects/10200/assetParents\"}", parsedTest);
+    }
+
+    @Test
+    public void shouldDeserializeObjectWithOverloadedSetterAndOverriddenProperties() {
+        Map<String, Object> test = new HashMap<String, Object>();
+        test.put("value", "ANY");
+        test.put("text","test");
+
+        String serialized = JSON.defaultJSON().forValue(test);
+
+        final TestJsonObject result = JSONParser.defaultJSONParser().parse(TestJsonObject.class, serialized);
+
+        assertEquals(TestJsonObject.Value.ANY, result.getValue());
+        assertEquals("test",result.getText());
+    }
+
+
+
+}
+
+
