60a8c2484ad5df2be50da8eab4ff3fe56cb075d9
Dariusz Kaczynski <dkac@softwareag.com>
2024-06-11 16:08:44 +0200

    MTM-59525 Creates pre-merge action

---
+++ .github/scripts/mvn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+MAVEN_ARGS="${MAVEN_ARGS:-} $@"
+VERBOSE=true
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -q | --quiet ) VERBOSE=false; shift ;;
+    * ) shift ;;
+  esac
+done
+
+if [ -n "${DEPENDENCIES_VERSION}" ]; then
+  MAVEN_ARGS="--define cumulocity-dependencies.version=${DEPENDENCIES_VERSION} ${MAVEN_ARGS}"
+fi
+
+MAVEN_PROFILES="${MAVEN_PROFILES:-ci}"
+MAVEN_ARGS="--activate-profiles ${MAVEN_PROFILES} ${MAVEN_ARGS}"
+
+if [ -n "${WORKSPACE}" ]; then
+  MAVEN_ARGS="--define maven.repo.local=${WORKSPACE}/.m2/repository ${MAVEN_ARGS}"
+fi
+
+MVN_SETTINGS="${MVN_SETTINGS:-$HOME/.m2/settings.xml}"
+MAVEN_ARGS="--settings ${MVN_SETTINGS} ${MAVEN_ARGS}"
+
+if [ ${VERBOSE} = true ]; then
+  MAVEN_ARGS="--show-version --errors ${MAVEN_ARGS}"
+  set -x
+fi
+
+export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=192m ${MAVEN_OPTS}"
+
+./mvnw --batch-mode --threads 4 ${MAVEN_ARGS}
