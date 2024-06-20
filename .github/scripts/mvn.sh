1ad39aba5de895cb8275e18eeb1b6dfa33351785
Dariusz Kaczynski <dkac@softwareag.com>
2024-06-20 09:42:27 +0200

    Adapts mvn.sh to Maven 3.9+

--- .github/scripts/mvn.sh
+++ .github/scripts/mvn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-MAVEN_ARGS="${MAVEN_ARGS:-} $@"
+MAVEN_CLI_ARGS="${MAVEN_CLI_ARGS:-} $@"
 VERBOSE=true
 
 while [ $# -gt 0 ]; do
@@ -11,33 +11,34 @@ while [ $# -gt 0 ]; do
 done
 
 if [ -n "${VERSION}" ]; then
-  MAVEN_ARGS="--define revision=${VERSION} ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--define revision=${VERSION} ${MAVEN_CLI_ARGS}"
   if [ -n "${CHANGE_VERSION}" ]; then
-    MAVEN_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_CLI_ARGS}"
   else
-    MAVEN_ARGS="--define changelist= ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist= ${MAVEN_CLI_ARGS}"
   fi
 else
   if [ -n "${CHANGE_VERSION}" ]; then
-    MAVEN_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_CLI_ARGS}"
   fi
 fi
 
 MAVEN_PROFILES="${MAVEN_PROFILES:-ci}"
-MAVEN_ARGS="--activate-profiles ${MAVEN_PROFILES} ${MAVEN_ARGS}"
+MAVEN_CLI_ARGS="--activate-profiles ${MAVEN_PROFILES} ${MAVEN_CLI_ARGS}"
 
 if [ -n "${WORKSPACE}" ]; then
-  MAVEN_ARGS="--define maven.repo.local=${WORKSPACE}/.m2/repository ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--define maven.repo.local=${WORKSPACE}/.m2/repository ${MAVEN_CLI_ARGS}"
 fi
 
 MVN_SETTINGS="${MVN_SETTINGS:-$HOME/.m2/settings.xml}"
-MAVEN_ARGS="--settings ${MVN_SETTINGS} ${MAVEN_ARGS}"
+MAVEN_CLI_ARGS="--settings ${MVN_SETTINGS} ${MAVEN_CLI_ARGS}"
 
 if [ ${VERBOSE} = true ]; then
-  MAVEN_ARGS="--show-version --errors ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--show-version --errors ${MAVEN_CLI_ARGS}"
   set -x
 fi
 
+# https://maven.apache.org/configure.html
 export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=192m ${MAVEN_OPTS}"
 
-./mvnw --batch-mode --threads 4 ${MAVEN_ARGS}
+./mvnw --batch-mode --threads 4 ${MAVEN_CLI_ARGS}
